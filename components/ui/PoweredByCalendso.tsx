eb3e60742c1245825c5ff3294d711e7da0553f44
root <root@production-builder-0.us-west1-b.c.railway-infra.internal>
2021-09-24 04:46:26 +0000

    Initial Commit

---
+++ components/ui/PoweredByCalendso.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+const PoweredByCalendso = () => (
+  <div className="text-xs text-center sm:text-right p-1">
+    <Link href={`https://cal.com?utm_source=embed&utm_medium=powered-by-button`}>
+      <a target="_blank" className="dark:text-white text-gray-500 opacity-50 hover:opacity-100">
+        powered by{" "}
+        <img className="dark:hidden w-auto inline h-[10px] relative" src="/logo.svg" alt="Cal.com Logo" />
+        <img
+          className="hidden dark:inline w-auto h-[10px] relative"
+          src="/logo-white.svg"
+          alt="Cal.com Logo"
+        />
+      </a>
+    </Link>
+  </div>
+);
+
+export default PoweredByCalendso;
